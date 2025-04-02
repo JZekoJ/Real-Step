3e3805b2d7d9544bb4ed7537cd313259373757cd
doriansimonet <dsimonet@gaming.tech>
2025-04-02 11:23:45 +0200

    Update : spawn Portal WIP

--- Assets/Script/Tools/SpawnerPortal.cs
+++ Assets/Script/Tools/SpawnerPortal.cs
@@ -19,7 +19,9 @@ public class SpawnerPortal : MonoBehaviour
         if(t>1)
         {
             GameObject portal = Portals[Random.Range(0, Portals.Count)];
-            Vector3 loc = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+            Vector3 loc = new Vector3(Random.Range(-5.0f, 5.0f), 0.5f, Random.Range(-5.0f, 5.0f));
+            Vector3 rot = new Vector3(portal.transform.rotation.x, portal.transform.rotation.y, Random.Range(0.0f, 360.0f));
+            portal.transform.Rotate(rot);
             Instantiate(portal, loc, portal.transform.rotation);
             t = 0;
         }
