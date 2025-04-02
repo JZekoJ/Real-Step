58bf033267d17460be27dadfcb0d9091f44b6acb
roidescrabes <epulzatto@gaming.tech>
2025-04-02 20:03:23 +0200

    update:now we know  where the swipe direction is youhou 🐻🐻

--- Assets/Scripts/InputSystem/InputManager.cs
+++ Assets/Scripts/InputSystem/InputManager.cs
@@ -1,9 +1,8 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [DefaultExecutionOrder(-1)]
-public class InputManager : MonoBehaviour
+public class InputManager : Singleton<InputManager>
 {
    
     public delegate void StartTouchEvent(Vector2 position, float time);
@@ -33,32 +32,31 @@ public class InputManager : MonoBehaviour
 
     private void Start()
     {
-        inputSystem.Touch.TouchPress.started += ctx => StartTouch(ctx);
-        inputSystem.Touch.TouchPress.canceled += ctx => EndTouch(ctx);
+        inputSystem.Touch.PrimaryContact.started += ctx => StartTouch(ctx);
+        inputSystem.Touch.PrimaryContact.canceled += ctx => EndTouch(ctx);
     }
 
     private void StartTouch(InputAction.CallbackContext context)
     {
-        Debug.Log("Touch started" + inputSystem.Touch.TouchPosition.ReadValue<Vector2>());
+       
         if (OnStartTouch != null)
         {
-            OnStartTouch(Utils.ScreenToWorld(mainCamera, inputSystem.Touch.TouchPosition.ReadValue<Vector2>()),(float)context.startTime);
+            OnStartTouch(Utils.ScreenToWorld(mainCamera, inputSystem.Touch.PrimaryPosition.ReadValue<Vector2>()),(float)context.startTime);
         }
     }
 
     private void EndTouch(InputAction.CallbackContext context)
     {
-        
-        Debug.Log("Touch end at position: " + inputSystem.Touch.TouchPosition.ReadValue<Vector2>());
+
         if (OnEndTouch != null)
         {
-            OnEndTouch(Utils.ScreenToWorld(mainCamera, inputSystem.Touch.TouchPosition.ReadValue<Vector2>()), (float)context.startTime);
+            OnEndTouch(Utils.ScreenToWorld(mainCamera, inputSystem.Touch.PrimaryPosition.ReadValue<Vector2>()), (float)context.startTime);
         }
     }
 
     public Vector2 PrimaryPosition()
     {
-        return Utils.ScreenToWorld(mainCamera, inputSystem.Touch.TouchPosition.ReadValue<Vector2>());
+        return Utils.ScreenToWorld(mainCamera, inputSystem.Touch.PrimaryPosition.ReadValue<Vector2>());
     }
 
 
