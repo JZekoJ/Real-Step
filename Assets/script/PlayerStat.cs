373211507c300449d0b6e071dc70dd1c65e4e528
doriansimonet <dsimonet@gaming.tech>
2025-04-04 17:03:55 +0200

    update : start player stat

---
+++ Assets/script/PlayerStat.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat : MonoBehaviour
+{
+
+    private int m_iVitality{ get; set; }
+    private int m_iStrenght { get; set; }
+    private int m_iMind { get; set; }
+    private int m_iIntelligence { get; set; }
+    private int m_iDexterity { get; set; }
+    private int m_iCharisma { get; set; }
+    private float m_fExperience { get; set; }
+    private int m_iLevel { get; set; }
+    // sa liste d'équipement ?
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    //add equipment (faire un truc classique)
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LevelUp()
+    {
+
+    }
+
+    public void ActualStrenght()
+    {
+        //est ce que je fais strenght + arme strnght = Actual strength ?
+    }
+    
+}
---
+++ Assets/script/Weapon.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    private int m_iStrenght { get; set; }
+    // Start is called before the first frame update
+    //equiper faire en sorte que quand on clique sur l'îcone ça affiche que les mêmes trucs, fin démerdes toi
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
