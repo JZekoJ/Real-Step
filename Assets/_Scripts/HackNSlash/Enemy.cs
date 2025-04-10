1edabaab71241dd2e1a9c8d2f1171033b95be0f6
ZekoJ <lorisfiard1@gmail.com>
2025-04-10 14:26:51 +0200

    dungeon spawner

--- Assets/_Scripts/HackNSlash/Enemy.cs
+++ Assets/_Scripts/HackNSlash/Enemy.cs
@@ -3,51 +3,40 @@ using UnityEngine.Events;
 
 public class Enemy : MonoBehaviour
 {
-    // Variables de base
     [Header("Statistiques")]
     [SerializeField] private int maxHealth = 100;
     [SerializeField] private int currentHealth;
     [SerializeField] private int attackPower = 10;
     [SerializeField] private int defense = 5;
-
-    // Événement pour notifier de la mort
+    
     public UnityEvent onDeath;
 
     private void Awake()
     {
-        // Initialiser la santé au démarrage
         currentHealth = maxHealth;
         
-        // Initialiser l'événement s'il est null
         if (onDeath == null)
             onDeath = new UnityEvent();
     }
-
-    // Fonction simple pour prendre des dégâts
+    
     public void TakeDamage(int damage)
     {
-        // Réduire les points de vie directement
         currentHealth -= damage;
         
-        // Afficher les HP dans la console
         Debug.Log(gameObject.name + " a pris " + damage + " dégâts. HP restants: " + currentHealth + "/" + maxHealth);
         
-        // Vérifier si l'ennemi est mort
         if (currentHealth <= 0)
         {
             Die();
         }
     }
-
-    // Fonction appelée quand l'ennemi meurt
+    
     private void Die()
     {
         Debug.Log(gameObject.name + " est mort!");
         
-        // Déclencher l'événement de mort
         onDeath.Invoke();
         
-        // Détruire le GameObject
         Destroy(gameObject);
     }
     
