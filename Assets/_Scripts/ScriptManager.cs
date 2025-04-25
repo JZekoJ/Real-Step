633737411900abe2b1e6025953e9b342189b7652
FIVEDingus <dbeney@gaming.tech>
2025-04-25 15:30:19 +0200

    update

--- Assets/_Scripts/ScriptManager.cs
+++ Assets/_Scripts/ScriptManager.cs
@@ -6,14 +6,12 @@ public class ScriptManager : MonoBehaviour
 {
     public static ScriptManager Instance;
 
-    public ShopSystem shopSystem;
     public MoneySystem moneySystem;
     public InventoryScript inventory;
 
     private void Awake()
     {
-        shopSystem.moneySystem = moneySystem.GetComponent<MoneySystem>();
-        shopSystem.inventory = inventory.GetComponent<InventoryScript>();
+
     }
 }
 
