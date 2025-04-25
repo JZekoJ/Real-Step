481705d9e746fb7091e3b95ae11f0ecc12931968
roidescrabes <epulzatto@gaming.tech>
2025-04-25 17:51:19 +0200

    update

--- Assets/_Scripts/Portal/PortalEnemies.cs
+++ Assets/_Scripts/Portal/PortalEnemies.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class PortalEnemies : MonoBehaviour
 {
     [Header("Paramètres de génération")]
-    [SerializeField] private List<GameObject> m_lAvailableEnemyPrefabs;
+    [SerializeField] private EnemyGenerator m_csEnemyGenerator;
     [SerializeField] private int m_iEnemyCount = 3;
 
     private List<GameObject> m_lGeneratedEnemies = new List<GameObject>();
@@ -13,7 +13,13 @@ public class PortalEnemies : MonoBehaviour
 
     private void Awake()
     {
-        EnemyGenerator generator = new EnemyGenerator();
-        m_lGeneratedEnemies = generator.Generate(m_lAvailableEnemyPrefabs, m_iEnemyCount);
+        if (m_csEnemyGenerator == null)
+        {
+            Debug.LogError("EnemyGenerator non assignéPortalEnemies ");
+            return;
+        }
+
+        // Génère des ennemis sans boss ici
+        m_lGeneratedEnemies = m_csEnemyGenerator.GenerateRoomEnemies(m_iEnemyCount, false);
     }
 }
