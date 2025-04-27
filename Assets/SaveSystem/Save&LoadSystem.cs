f0a963155542f13a9e2d6c9b26a2628af87d29ff
doriansimonet <dsimonet@gaming.tech>
2025-04-27 16:50:29 +0200

    Update : Add UI

--- Assets/SaveSystem/Save&LoadSystem.cs
+++ Assets/SaveSystem/Save&LoadSystem.cs
@@ -12,7 +12,7 @@ public class SaveLoadSystem
         {
             string playerDataPath = Application.persistentDataPath + "/playerData.json";
             string json = JsonConvert.SerializeObject(player.PlayerData, Formatting.Indented);
-            Debug.Log(json);
+            Debug.Log(playerDataPath);
             File.WriteAllText(playerDataPath, json);
         }
     }
@@ -27,7 +27,7 @@ public class SaveLoadSystem
             if (File.Exists(playerDataPath))
             {
                 string json = File.ReadAllText(playerDataPath);
-                //Debug.Log(json);
+                Debug.Log(json);
                 player.PlayerData = JsonConvert.DeserializeObject<PlayerData>(json, new JsonSerializerSettings() { ObjectCreationHandling = ObjectCreationHandling.Replace });
             }
             else
