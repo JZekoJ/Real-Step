a1d6afe68f06413b33bcdba813daeb958f2983b4
doriansimonet <dsimonet@gaming.tech>
2025-04-28 01:34:01 +0200

    Update : Link UI Sound

--- Assets/_Scripts/Audio/AudioSettingsUI.cs
+++ Assets/_Scripts/Audio/AudioSettingsUI.cs
@@ -1,34 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class AudioSettingsUI : MonoBehaviour
 {
-    public Slider masterSlider;
+    //public Slider masterSlider;
     public Slider musicSlider;
     public Slider sfxSlider;
 
-    private void Start()
+    private void OnEnable()
     {
-        float master = PlayerPrefs.GetFloat("MasterVolume", 1f);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        
+    }
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("ICI : " + AudioManager.Instance);
+        foreach (Slider slide in Resources.FindObjectsOfTypeAll(typeof(Slider)) as Slider[])
+        {
+            Debug.Log(slide.gameObject);
+            if (slide.gameObject.name == "Son")
+            {
+                sfxSlider = slide;
+            }
+            if (slide.gameObject.name == "Musique")
+            {
+                musicSlider = slide;
+            }
+        }
+        float master = PlayerPrefs.GetFloat("MasterVolume",1f);
         float music = PlayerPrefs.GetFloat("MusicVolume", 1f);
         float sfx = PlayerPrefs.GetFloat("SFXVolume", 1f);
 
-        masterSlider.value = master;
+        //masterSlider.value = master;
         musicSlider.value = music;
         sfxSlider.value = sfx;
 
-        masterSlider.onValueChanged.AddListener(AudioManager.Instance.SetMasterVolume);
+        //masterSlider.onValueChanged.AddListener(AudioManager.Instance.SetMasterVolume);
         musicSlider.onValueChanged.AddListener(AudioManager.Instance.SetMusicVolume);
         sfxSlider.onValueChanged.AddListener(AudioManager.Instance.SetSFXVolume);
+    }
+
+    private void Start()
+    {
+        
+        
+        
     }
 
     private void OnDestroy()
     {
         if (AudioManager.Instance != null)
         {
-            masterSlider.onValueChanged.RemoveListener(AudioManager.Instance.SetMasterVolume);
+            //masterSlider.onValueChanged.RemoveListener(AudioManager.Instance.SetMasterVolume);
             musicSlider.onValueChanged.RemoveListener(AudioManager.Instance.SetMusicVolume);
             sfxSlider.onValueChanged.RemoveListener(AudioManager.Instance.SetSFXVolume);
         }
