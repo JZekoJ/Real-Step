0a9e8e9ff6309a3d652b6c34112cf7490242b5d7
doriansimonet <dsimonet@gaming.tech>
2025-04-28 17:13:26 +0200

    Update : Stat in SHop

--- Assets/_Scripts/Shop/ItemToBuyUI.cs
+++ Assets/_Scripts/Shop/ItemToBuyUI.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEditor.Progress;
 
 public class ItemToBuyUI : MonoBehaviour
 {
     public Item _item;
     private int _price;
+
+    #region UI
+    public GameObject m_gStatUIParent;
+    public GameObject m_gStatUIPrefab;
+    public GameObject m_gImageItem;
+    #endregion
     public void Init()
     {
         ReferenceManager.Player.PlayerData._iMoney += 10000;
@@ -20,6 +27,8 @@ public class ItemToBuyUI : MonoBehaviour
         GetComponentInChildren<TextMeshProUGUI>().text = "" + _price;
 
         Debug.Log(GetComponentsInChildren<Button>()[0].name);
+
+        GetComponentsInChildren<Button>()[0].onClick.AddListener(()=>ShowStat());
         GetComponentsInChildren<Button>()[1].onClick.AddListener(() =>
         {
             //Buy
@@ -34,5 +43,75 @@ public class ItemToBuyUI : MonoBehaviour
             GetComponentsInChildren<Button>()[1].onClick.RemoveAllListeners();
 
         });
+    }
+
+    public void ShowStat() {
+
+        m_gImageItem.GetComponentInChildren<ItemUIScript>()._EquipItem = _item;
+        m_gImageItem.GetComponentInChildren<ItemUIScript>().Init();
+        foreach (RectTransform rtStat in m_gStatUIParent.GetComponentsInChildren<RectTransform>())
+        {
+            if (rtStat != m_gStatUIParent.GetComponentsInChildren<RectTransform>()[0])
+            {
+                Destroy(rtStat.gameObject);
+            }
+        }
+
+        for (int i = 0; i < _item._Stats.Count; i++)
+        {
+
+            string stat = "";
+            switch ((ItemStats)i)
+            {
+                case ItemStats.Strength:
+                    stat = "Strenght : ";
+                    break;
+                case ItemStats.Spirit:
+                    stat = "Spirit : ";
+                    break;
+                case ItemStats.Intel:
+                    stat = "Int : ";
+                    break;
+                case ItemStats.Vita:
+                    stat = "Health : ";
+                    break;
+                case ItemStats.Char:
+                    stat = "Char : ";
+                    break;
+                case ItemStats.Dext:
+                    stat = "Dext : ";
+                    break;
+            }
+
+            Debug.Log(i);
+            GameObject textStat = Instantiate(m_gStatUIPrefab, m_gStatUIParent.transform);
+            textStat.GetComponentsInChildren<TextMeshProUGUI>()[0].text = stat + _item._Stats[i].ToString();
+            int diff = _item._Stats[i];
+            
+            if (ReferenceManager.Player.PlayerData.ItemSlot[(int)_item._Type] != null)
+            {
+                //ReferenceManager.Player.PlayerData.ItemSlot[(int)item._Type]._bIsEquiped = false;
+                Debug.Log("???");
+                diff = _item._Stats[i] - ReferenceManager.Player.PlayerData.ItemSlot[(int)_item._Type]._Stats[i];// ReferenceManager.Player.PlayerData.ItemSlot[(int)item._Type]._Stats[i]
+            }
+            string moreless = "";
+            if (diff > 0)
+            {
+                moreless = " + ";
+                textStat.GetComponentsInChildren<TextMeshProUGUI>()[1].color = Color.green;
+            }
+            if (diff == 0)
+            {
+                moreless = "";
+                textStat.GetComponentsInChildren<TextMeshProUGUI>()[1].color = Color.white;
+            }
+            if (diff < 0)
+            {
+                moreless = "";
+                textStat.GetComponentsInChildren<TextMeshProUGUI>()[1].color = Color.red;
+            }
+
+            textStat.GetComponentsInChildren<TextMeshProUGUI>()[1].text = moreless + diff.ToString();
+        }
     }
 }
