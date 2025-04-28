b2293578772f0fc548a8c1657515560ad5f73cbb
doriansimonet <dsimonet@gaming.tech>
2025-04-28 17:13:26 +0200

    Update : Stat in SHop

--- Assets/_Scripts/Shop/Shop.cs
+++ Assets/_Scripts/Shop/Shop.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class Shop : MonoBehaviour
 {
     public GameObject ItemPrefab;
+    public GameObject StatParent;
+    public GameObject ImageItem;
 
     private void Start()
     {
         for (int i = 0; i < 9; i++)
         {
             GameObject itemToBuy = Instantiate(ItemPrefab, this.transform);
+            itemToBuy.GetComponent<ItemToBuyUI>().m_gStatUIParent = StatParent;
+            itemToBuy.GetComponent<ItemToBuyUI>().m_gImageItem = ImageItem;
             itemToBuy.GetComponent<ItemToBuyUI>().Init();
+
         }
     }
 }
